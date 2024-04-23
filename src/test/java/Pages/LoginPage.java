74125c67d7d9ffd9a4c6f8373a20aaa20db44087
Dima Lis <iselis777888999@gmail.com>
2024-04-23 21:27:34 +0300

    first commit

---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import Data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.*;
+public class LoginPage {
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        $("[data-test-id='login'] input").setValue(info.getLogin());
+        $("[data-test-id='password'] input").setValue(info.getPassword());
+        $("button.button").click();
+        return new VerificationPage();
+    }
+}
+
